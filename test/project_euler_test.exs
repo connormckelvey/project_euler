63c3049abb590577edbf8625fc9b28e1d96b7310
Connor McKelvey <connormckelvey@gmail.com>
2016-10-13 17:07:32 -0600

    Added tests

--- test/project_euler_test.exs
+++ test/project_euler_test.exs
@@ -1,8 +1,14 @@
 defmodule ProjectEulerTest do
   use ExUnit.Case
   doctest ProjectEuler
+  alias ProjectEuler.ProblemOne
+  alias ProjectEuler.ProblemTwo
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Problem One" do
+    assert ProblemOne.solve == 233168
+  end
+
+  test "Problem Two" do
+    assert ProblemTwo.solve == 4613732
   end
 end
