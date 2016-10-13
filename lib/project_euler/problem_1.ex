29b43e2c2eccecc09b7b206ef40796f3f49aca1b
Connor McKelvey <connormckelvey@gmail.com>
2016-10-13 17:03:08 -0600

    Reformatting

--- lib/project_euler/problem_1.ex
+++ lib/project_euler/problem_1.ex
@@ -1,16 +1,13 @@
 defmodule ProjectEuler.ProblemOne do
-  defmodule SolutionOne do
-    def solve do
-      Enum.reduce(0..999, 0, &reducer/2)
-    end
+  def solve do
+    Enum.reduce(0..999, 0, &reducer/2)
+  end
 
-    defp reducer(number, acc) when rem(number, 3) == 0 or rem(number, 5) == 0 do
-      acc + number
-    end
+  defp reducer(number, acc) when rem(number, 3) == 0 or rem(number, 5) == 0 do
+    acc + number
+  end
 
-    defp reducer(_, acc) do
-      acc
-    end
-
+  defp reducer(_, acc) do
+    acc
   end
 end
